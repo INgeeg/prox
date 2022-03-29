607f68be9f599bd0defb10902a95225f1e91d3bb
Ingeeg <aajk2008@gmail.com>
2022-03-29 14:33:34 -0500

    cosmosmongo template

---
+++ cosmosmongo-webapi/Services/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using cosmosmongo_webapi.Models;
+
+namespace cosmosmongo_webapi.Services
+{
+    public interface IProductService
+    {
+        public Task<List<Product>> GetNAsync(int n);
+        public Task<Product> GetBySkuAsync(string sku);
+        public Task CreateAsync(Product product);
+        public Task<Product> UpdateAsync(Product product);
+        public Task DeleteAsync(string sku);
+    }
+}
