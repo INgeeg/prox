65165ef9e3d36d5d6d94b1d0c909d9e0f36e8ddf
Your Name <aajk2008@gmail.com>
2022-05-18 22:49:30 -0500

    kafka topic change

--- kafka_example/KafkaCSharp/ApacheKafkaProducerDemo/Controllers/ProducerController.cs
+++ kafka_example/KafkaCSharp/ApacheKafkaProducerDemo/Controllers/ProducerController.cs
@@ -10,7 +10,7 @@ namespace ApacheKafkaProducerDemo.Controllers;
 [ApiController]
 public class ProducerController: ControllerBase {
     private readonly string bootstrapServers = "localhost:9092";
-    private readonly string topic = "test";
+    private readonly string topic = "test1";
 
     [HttpPost]
     public async Task <IActionResult> Post([FromBody] OrderRequest orderRequest) {
